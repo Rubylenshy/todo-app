cf8abb1e5ae099d2e025f916aef146b71a14e5cb
Rubylenshy <reztomoloju@gmail.com>
2022-10-20 22:12:45 +0100

    todo-task checked feature

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,10 @@ const lightIcon = document.querySelector('img[alt="Sun"]')
 const darkIcon = document.querySelector('img[alt="Moon"]')
 
 const todoBody = document.querySelector('.todo-body-list')
-const radioBtn = document.querySelectorAll('.radio-btn')
+const radioBtn = todoBody.querySelectorAll('.radio-btn')
 const text = todoBody.querySelectorAll('.task')
+const todoItem = todoBody.querySelectorAll('.todo-item')
+const itemNumber = document.querySelector('#items-left');
 
 const html = document.querySelector('html')
 function dark(){
@@ -19,6 +21,15 @@ function light(){
     lightIcon.style.display = 'none';
     lightIcon.classList.add('rotate')
     darkIcon.style.display = 'block';
+}
+function deductItem(){
+    var num;
+        num = todoItem.length;
+        newDefault = num - 1;
+        itemNumber.textContent = newDefault;
+}
+function itemsLeft(){
+    itemNumber.textContent = todoItem.length;
 }
 
 toggler.addEventListener('click', ()=>{
@@ -35,21 +46,41 @@ todoBody.addEventListener('click', (remove)=>{
     if (remove.target.className == 'cancel') {
         const div = remove.target.parentElement;
         div.parentNode.removeChild(div);
+
+        deductItem();
     }
+    
 });
 
 for (let i = 0; i < radioBtn.length; i++) {
     
     radioBtn[i].addEventListener('click', ()=>{
-        
 
         if (radioBtn[i].classList.contains('checked') == false) {
             radioBtn[i].classList.add('checked');
-            document.getElementsByClassName('task').style.textDecoration = 'line-through';
+            text[i].classList.add('line')
+            
+            deductItem();
         }else{
             radioBtn[i].classList.remove('checked');
-            text[i].style.textDecoration = 'none'
+            text[i].classList.remove('line')
+
+            itemNumber.textContent = newDefault + 1;
         }
      })
 
+     todoItem[i].addEventListener('dblclick', ()=>{
+        todoItem[i].classList.add('dbclick');
+     })
+     
+     todoItem[i].addEventListener('mouseup', ()=>{
+        todoItem[i].classList.remove('dbclick');
+     })
+
+     todoItem[i].addEventListener('drag', ()=>{
+        todoItem[i].classList.add('dbclick');
+     })
+
 }
+
+window.addEventListener('load', itemsLeft);
